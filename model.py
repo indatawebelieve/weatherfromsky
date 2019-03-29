341c41cbbbbbd4a628a358b7419d69c1cb3f78e1
Joaquin Sanchez <sanchezjoaquin1995@gmail.com>
2019-03-29 16:58:29 -0300

    Basic model

--- model.py
+++ model.py
@@ -18,68 +18,73 @@ import os
 import cv2
 import matplotlib.pyplot as plt
 
-tf.enable_eager_execution()
+#tf.enable_eager_execution()
 
 img_num_channels = 3
 img_width_height = 192
 input_dir = './input/imgs/'
 
+num_classes = 2
 classes = []
 paths = []
 folders = os.listdir(input_dir)
-for i in range(0, len(folders)):
-	folder = folders[i]
+classindex = 0
+for folder in folders:
 	if '.DS_Store' not in folder: # osx automatically created file
 		files = os.listdir(os.path.join(input_dir, folder))
 
 		for file in files:
 			if '.DS_Store' not in file: # osx automatically created file
 				paths.append(os.path.join(input_dir, folder, file))
-				classes.append(i-1)
+				classes.append(classindex)
 
-train_length = len(paths) * 0.8
-filenames = tf.constant(paths)
-labels = tf.constant(classes)
+		classindex += 1
 
-# Create the dataset
-dataset = tf.data.Dataset.from_tensor_slices((filenames, labels))
-def read_files(filename, label):
-    image = tf.read_file(filename)
-    image = tf.image.decode_jpeg(image, channels=img_num_channels)
-    # All images must have the same size
-    image = tf.image.resize_images(image, size=[img_width_height, img_width_height], method=tf.image.ResizeMethod.BILINEAR)
-    image = tf.cast(image, tf.float32)
-    image /= 255.0
-    
-    return image, label
+train_x = []
+train_y = []
+for i in range(0, len(paths)):
+	path = paths[i]
 
-dataset = dataset.map(read_files)
-dataset = dataset.shuffle(buffer_size=len(paths)) # Add some randomless
-# Split into training and testing
-trainds = dataset.take(int(train_length))
-testds = dataset.skip(int(train_length))
+	image = cv2.imread(path)
+	image = cv2.resize(image, (img_width_height, img_width_height), 0, 0, cv2.INTER_LINEAR)
+	image = image.astype(np.float32)
+	image = np.multiply(image, 1.0 / 255.0)
 
-def show_image():
-	for n,(image, label) in enumerate(trainds.take(1)):
-		plt.imshow(image)
-		plt.show()
+	train_x.append(image)
+	train_y.append(classes[i])
 
-trainds = trainds.repeat()
-trainds = trainds.batch(32)
+train_x = np.array(train_x).reshape(-1, img_width_height, img_width_height, 3)
+train_y = np.array(train_y)
+
+print(train_y)
 
 # define model
 model = tf.keras.Sequential()
 # Input layer must know which is the input shape
-model.add(tf.keras.layers.Dense(2, activation=tf.nn.relu, input_shape=[img_width_height, img_width_height, img_num_channels]))
-model.compile(loss='categorical_crossentropy', optimizer=tf.train.AdamOptimizer(), metrics=['accuracy'])
+model.add(tf.keras.layers.Conv2D(256, (3,3), input_shape=train_x.shape[1:]))
+model.add(tf.keras.layers.Activation('relu'))
+model.add(tf.keras.layers.MaxPooling2D(pool_size=(2,2)))
+
+model.add(tf.keras.layers.Conv2D(256, (3, 3)))
+model.add(tf.keras.layers.Activation('relu'))
+model.add(tf.keras.layers.MaxPooling2D(pool_size=(2,2)))
+
+model.add(tf.keras.layers.Flatten())
+
+model.add(tf.keras.layers.Dense(64))
+model.add(tf.keras.layers.Dense(1))
+model.add(tf.keras.layers.Activation('sigmoid'))
+
+# model.compile(loss='sparse_categorical_crossentropy', optimizer=tf.train.AdamOptimizer(), metrics=['accuracy'])
+model.compile(loss='binary_crossentropy', optimizer=tf.train.AdamOptimizer(), metrics=['accuracy'])
 
 print(len(model.trainable_variables))
 print(model.summary())
 
 
-'''
 model.fit(train_x, train_y, epochs=10)
 
+'''
 # show training results
 scores = model.predict(test_x)
 print(np.argmax(scores)) '''

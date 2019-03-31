f7c4c4cb6b11db7c37ba6285e3a4f452a71505c3
Joaquin Sanchez <sanchezjoaquin1995@gmail.com>
2019-03-31 01:29:22 -0300

    First complete model

--- model.py
+++ model.py
@@ -18,13 +18,11 @@ import os
 import cv2
 import matplotlib.pyplot as plt
 
-#tf.enable_eager_execution()
-
 img_num_channels = 3
 img_width_height = 192
-input_dir = './input/imgs/'
+input_dir = os.path.join('.', 'input', 'imgs')
 
-num_classes = 2
+num_classes = 3
 classes = []
 paths = []
 folders = os.listdir(input_dir)
@@ -36,12 +34,14 @@ for folder in folders:
 		for file in files:
 			if '.DS_Store' not in file: # osx automatically created file
 				paths.append(os.path.join(input_dir, folder, file))
-				classes.append(classindex)
+				label = np.zeros(num_classes)
+				label[classindex] = 1.0
+				classes.append(label)
 
 		classindex += 1
 
-train_x = []
-train_y = []
+x = []
+y = []
 for i in range(0, len(paths)):
 	path = paths[i]
 
@@ -50,18 +50,27 @@ for i in range(0, len(paths)):
 	image = image.astype(np.float32)
 	image = np.multiply(image, 1.0 / 255.0)
 
-	train_x.append(image)
-	train_y.append(classes[i])
+	x.append(image)
+	y.append(classes[i])
 
-train_x = np.array(train_x).reshape(-1, img_width_height, img_width_height, 3)
+# Splitting into training and testing
+training_size = 0.8
+training_size = round(len(x)*training_size)
+
+train_x = x[:training_size]
+train_y = y[:training_size]
+train_x = np.array(train_x)
 train_y = np.array(train_y)
 
-print(train_y)
+test_x = x[training_size:]
+test_y = y[training_size:]
+test_x = np.array(test_x)
+test_y = np.array(test_y)
 
 # define model
 model = tf.keras.Sequential()
 # Input layer must know which is the input shape
-model.add(tf.keras.layers.Conv2D(256, (3,3), input_shape=train_x.shape[1:]))
+model.add(tf.keras.layers.Conv2D(256, (3,3), input_shape=(192, 192, 3)))
 model.add(tf.keras.layers.Activation('relu'))
 model.add(tf.keras.layers.MaxPooling2D(pool_size=(2,2)))
 
@@ -69,22 +78,28 @@ model.add(tf.keras.layers.Conv2D(256, (3, 3)))
 model.add(tf.keras.layers.Activation('relu'))
 model.add(tf.keras.layers.MaxPooling2D(pool_size=(2,2)))
 
+# Always flatten before fully connected layers
 model.add(tf.keras.layers.Flatten())
 
 model.add(tf.keras.layers.Dense(64))
-model.add(tf.keras.layers.Dense(1))
+
+# output layer must have the same units as number of classes
+model.add(tf.keras.layers.Dense(num_classes))
 model.add(tf.keras.layers.Activation('sigmoid'))
 
-# model.compile(loss='sparse_categorical_crossentropy', optimizer=tf.train.AdamOptimizer(), metrics=['accuracy'])
-model.compile(loss='binary_crossentropy', optimizer=tf.train.AdamOptimizer(), metrics=['accuracy'])
+# loss categorical_crossentropy needs labels of type [0 1 0] (here class 2 is the right one)
+model.compile(loss='categorical_crossentropy', optimizer=tf.train.AdamOptimizer(), metrics=['accuracy'])
 
-print(len(model.trainable_variables))
 print(model.summary())
 
-
+# train the model
+print('Training...')
 model.fit(train_x, train_y, epochs=10)
 
-'''
+# test the training
+print('Testing...')
+model.evaluate(test_x, test_y)
+
 # show training results
 scores = model.predict(test_x)
-print(np.argmax(scores)) '''
+print(np.argmax(scores))
